86d2551209a3a433ab85ba2d1e18c8721e1f8bf9
AjunPlusWin <403621656@qq.com>
2026-02-04 11:15:57 +0800

    feat:添加user查询函数

--- crud.py
+++ crud.py
@@ -1,6 +1,22 @@
-from sqlmodel import Session, select
-from models import UserCreate, User, UserUpdate
+import uuid
+
+from sqlmodel import Session, select, func
+from models import UserCreate, User, UserUpdate, Users
 from core.security import get_password_hash, verify_password
+
+
+def get_users(
+    *,
+    session: Session,
+    skip: int=0,
+    limit: int=100,
+    ) -> Users:
+    statement_count = select(func.count()).select_from(User)
+    count = session.exec(statement_count).one()
+    statement = select(User).offset(skip).limit(limit)
+    users = session.exec(statement).all()
+    return Users(data=users, count=count)
+
 
 
 def create_user(*, user_create: UserCreate, session: Session) -> User:
@@ -28,6 +44,11 @@ def update_user(*, user_update: UserUpdate, db_user:User, session: Session) -> U
 def get_user_by_email(*, email: str, session: Session) -> User | None:
     statement = select(User).where(User.email == email)
     db_user = session.exec(statement).first()
+    return db_user
+
+
+def get_user_by_id(*, user_id: uuid.UUID, session: Session) -> User | None:
+    db_user = session.get(User, user_id)
     return db_user
 
 
--- models.py
+++ models.py
@@ -16,6 +16,11 @@ class User(UserBase, table=True):
     hashed_password: str
 
 
+class Users(SQLModel):
+    data: list[User]
+    count: int
+
+
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=128)
 
