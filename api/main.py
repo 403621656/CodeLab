4346cfb6fe6c63d8bdee347c8d99f1600f4dbe6d
AjunPlus <403621656@qq.com>
2026-02-05 23:10:20 +0800

    feat：挂载token路由

--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from api.routes import users
+from api.routes import users, token
 
 
 router = APIRouter()
 
 
 router.include_router(users.router)
+router.include_router(token.router)
