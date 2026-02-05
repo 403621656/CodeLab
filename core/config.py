c3753e5340e8da4701aed679a94fe0d39e680cb3
AjunPlus <403621656@qq.com>
2026-02-05 20:43:57 +0800

    feat: 增加数据库连接

--- core/config.py
+++ core/config.py
@@ -1,5 +1,8 @@
 import secrets
+
+from pydantic import PostgresDsn, computed_field
 from pydantic_settings import SettingsConfigDict, BaseSettings
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -8,5 +11,24 @@ class Settings(BaseSettings):
         extra="ignore",
     )
     SECRET_KEY: str = secrets.token_urlsafe(32)
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+
+    @computed_field()
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
 
 settings = Settings()
