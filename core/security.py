4146d4a5e5fc5983a8ff0413f7a0657bdbece5dd
AjunPlus <403621656@qq.com>
2026-02-05 23:02:52 +0800

    Revert "feat：获得本地token"
    This reverts commit 2262d8332d92901daf64fa9ef91fb149e7f3cf7e.

--- core/security.py
+++ core/security.py
@@ -1,18 +1,9 @@
 from pwdlib import PasswordHash
 
-from config import settings
-
-
 password_hash = PasswordHash.recommended()
-
 
 def get_password_hash(password: str) -> str:
     return password_hash.hash(password)
 
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
-
-
-def get_token() -> str:
-    return settings.SECRET_KEY
