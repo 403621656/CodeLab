baa837bc2481320d20d7668dddbf985ec94ef97a
AjunPlus <403621656@qq.com>
2026-02-08 09:44:16 +0800

    feat：添加 TokenDeps

--- api/deps.py
+++ api/deps.py
@@ -1,9 +1,11 @@
 from sqlmodel import Session
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from collections.abc import Generator
 
 from core.db import engine
+from core.security import local_token
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -11,4 +13,18 @@ def get_db() -> Generator[Session, None, None]:
         yield session
 
 
+bearer = HTTPBearer(auto_error=False)
+
+
+def require_fixed_token(
+        cred: HTTPAuthorizationCredentials|None = Depends(bearer)
+) -> HTTPAuthorizationCredentials:
+    if cred is None or cred.scheme.lower() != "bearer":
+        raise HTTPException(status_code=401, detail="Missing token")
+    if cred.credentials != local_token:
+        raise HTTPException(status_code=403, detail="Invalid token")
+    return cred
+
+
+TokenDeps = Annotated[HTTPAuthorizationCredentials, Depends(require_fixed_token)]
 SessionDeps = Annotated[Session, Depends(get_db)]
