5f7cf51fbf57e1d8db8fabd5fa8affd40dae4de9
AjunPlus <403621656@qq.com>
2026-02-08 09:42:04 +0800

    feat:添加 token api

---
+++ api/routes/token.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from fastapi.security import HTTPAuthorizationCredentials
+
+from models import BearerToken
+from core.security import local_token
+from api.deps import TokenDeps
+
+
+router = APIRouter(prefix="/token", tags=["Token"])
+
+
+@router.get("/", response_model=BearerToken)
+async def read_token() -> BearerToken:
+    return BearerToken(token=local_token)
+
+
+@router.get("/test-token", response_model=HTTPAuthorizationCredentials)
+async def test_token(cred: TokenDeps) -> HTTPAuthorizationCredentials:
+    return cred
