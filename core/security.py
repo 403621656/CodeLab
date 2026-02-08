245a804c431ee05607c79d458eca670f5798c010
AjunPlus <403621656@qq.com>
2026-02-08 09:46:38 +0800

    feat：添加本地token

--- core/security.py
+++ core/security.py
@@ -1,9 +1,15 @@
 from pwdlib import PasswordHash
 
+from core.config import settings
+
+
 password_hash = PasswordHash.recommended()
+local_token = settings.SECRET_KEY
+
 
 def get_password_hash(password: str) -> str:
     return password_hash.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
